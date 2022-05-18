11f5cc4886fe84ec5770bb93bd79b88907ffa031
NH-Rifat <rifatr729@gmail.com>
2022-05-19 05:27:22 +0600

    todo deleted suucessfully

--- src/Pages/Home/Tasks/SingleTask/SingleTask.js
+++ src/Pages/Home/Tasks/SingleTask/SingleTask.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { FcFullTrash, FcTodoList, FcDocument } from 'react-icons/fc';
 import { IoIosCreate, IoIosTrash } from 'react-icons/io';
 
-const SingleTask = ({ todo }) => {
-  // console.log(todo);
-  const { task, description, start, end } = todo;
-  // console.log('single todo',todo);
+const SingleTask = ({ todo, handleDelete}) => {
+  // console.log(handleDelete);
+  const {_id, task, description, start, end } = todo;
+  // console.log('single todo',_id);
   return (
     <div className='card lg:max-w-lg bg-base-200 shadow-xl'>
       <div className='card-body text-center'>
@@ -14,8 +14,12 @@ const SingleTask = ({ todo }) => {
         <p>{description}</p>
         <div className='card-actions flex justify-between mt-5'>
           <div className='text-3xl flex justify-between'>
-            <IoIosTrash></IoIosTrash>
-            <IoIosCreate className='ml-4'></IoIosCreate>
+            <div className='cursor-pointer'>
+              <IoIosTrash onClick={()=>handleDelete(_id)}></IoIosTrash>
+            </div>
+            <div className=''>
+              <IoIosCreate className='ml-4'></IoIosCreate>
+            </div>
           </div>
           <div className=''>
             <button className='text-xsm capitalize bg-slate-800 text-white rounded-md p-2'>
--- src/Pages/Home/Tasks/Tasks.js
+++ src/Pages/Home/Tasks/Tasks.js
@@ -10,17 +10,34 @@ const Tasks = () => {
       .then((data) => setTodos(data));
   }, [todos]);
   // console.log(todos);
+
+  const handleDelete = (todoId) => {
+    // console.log(todoId);
+    if (todoId) {
+      const confirmation = window.confirm('Are you Sure to Delete?');
+      if (confirmation) {
+        fetch(`http://localhost:5000/todos/${todoId}`, {
+          method: 'DELETE',
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            const remainingTodo = todos.filter((todo) => todo._id !== todoId);
+            setTodos(remainingTodo);
+          });
+      }
+    }
+  };
   return (
     <div className='w-3/4 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-      {
-        todos.map(todo =><SingleTask
+      {todos.map((todo) => (
+        <SingleTask
           key={todo._id}
-          todo={todo}>
-          </SingleTask>)
-      }
+          todo={todo}
+          handleDelete={handleDelete}
+        ></SingleTask>
+      ))}
     </div>
   );
 };
 
 export default Tasks;
-
